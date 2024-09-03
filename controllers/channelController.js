fa7b150760912061772f328bc9f7886b4a871a6b
turtlebeasts <179turtleshells@gmail.com>
2024-09-03 19:01:26 +0530

    used connection pool

--- controllers/channelController.js
+++ controllers/channelController.js
@@ -1,126 +1,104 @@
-const db = require('../config/database')
-const socketManager = require('../middleware/socketIo')
+const db = require('../config/database');
+const socketManager = require('../middleware/socketIo');
 
 const add_channel = async (req, res) => {
-    const io = socketManager.getIO()
+    const io = socketManager.getIO();
     const { name } = req.body;
     const userId = req.userId; // ID of the user creating the channel
 
     if (!name) return res.status(400).send('Channel name is required.');
 
-    db.beginTransaction((err) => {
-        if (err) return res.status(500).send('Error starting transaction.');
+    const connection = await db.getConnection();
+    try {
+        await connection.beginTransaction();
 
-        db.query('INSERT INTO channels (name) VALUES (?)', [name], (err, result) => {
-            if (err) {
-                return db.rollback(() => {
-                    res.status(500).send('Error creating channel.');
-                });
-            }
+        const [result] = await connection.query('INSERT INTO channels (name) VALUES (?)', [name]);
+        const channelId = result.insertId;
 
-            const channelId = result.insertId;
+        await connection.query('INSERT INTO channel_users (channel_id, user_id) VALUES (?, ?)', [channelId, userId]);
 
-            db.query('INSERT INTO channel_users (channel_id, user_id) VALUES (?, ?)', [channelId, userId], (err) => {
-                if (err) {
-                    return db.rollback(() => {
-                        res.status(500).send('Error adding user to channel.');
-                    });
-                }
-
-                db.commit((err) => {
-                    if (err) {
-                        return db.rollback(() => {
-                            res.status(500).send('Error committing transaction.');
-                        });
-                    }
-                    res.status(200).send({ channelId });
-                    io.emit('channel_created')
-                });
-            });
-        });
-    });
+        await connection.commit();
+        res.status(200).send({ channelId });
+        io.emit('channel_created');
+    } catch (err) {
+        await connection.rollback();
+        console.error('Error in add_channel:', err);
+        res.status(500).send('Error creating channel.');
+    } finally {
+        connection.release();
+    }
 }
 
 const get_channel_list = async (req, res) => {
     const userId = req.userId;
     const query = 'SELECT * FROM channel_users INNER JOIN channels ON channel_users.channel_id = channels.id WHERE channel_users.user_id = ?';
-    // const query = 'SELECT * FROM channels';
-    
-    db.query(query, [userId], (err, results) => {
-        if (err) return res.status(500).send('Error fetching channels.');
+
+    try {
+        const [results] = await db.query(query, [userId]);
         res.json(results);
-    });
+    } catch (err) {
+        console.error('Error in get_channel_list:', err);
+        res.status(500).send('Error fetching channels.');
+    }
 };
 
-
-const delete_channel = (req, res) => {
-    const io = socketManager.getIO()
+const delete_channel = async (req, res) => {
+    const io = socketManager.getIO();
     const channelId = req.params.channelId;
     if (!channelId) return res.status(400).send('Channel ID is required.');
 
-    db.beginTransaction((err) => {
-        if (err) return res.status(500).send('Error starting transaction.');
+    const connection = await db.getConnection();
+    try {
+        await connection.beginTransaction();
 
-        // First, delete the channel-users associations
-        db.query('DELETE FROM channel_users WHERE channel_id = ?', [channelId], (err) => {
-            if (err) {
-                return db.rollback(() => {
-                    res.status(500).send('Error removing users from channel.');
-                });
-            }
+        await connection.query('DELETE FROM channel_users WHERE channel_id = ?', [channelId]);
+        await connection.query('DELETE FROM channels WHERE id = ?', [channelId]);
 
-            // Then, delete the channel itself
-            db.query('DELETE FROM channels WHERE id = ?', [channelId], (err) => {
-                if (err) {
-                    return db.rollback(() => {
-                        res.status(500).send('Error deleting channel.');
-                    });
-                }
-
-                db.commit((err) => {
-                    if (err) {
-                        return db.rollback(() => {
-                            res.status(500).send('Error committing transaction.');
-                        });
-                    }
-                    res.status(200).send('Channel deleted successfully.');
-                    io.emit('channel_delete')
-                });
-            });
-        });
-    });
+        await connection.commit();
+        res.status(200).send('Channel deleted successfully.');
+        io.emit('channel_delete');
+    } catch (err) {
+        await connection.rollback();
+        console.error('Error in delete_channel:', err);
+        res.status(500).send('Error deleting channel.');
+    } finally {
+        connection.release();
+    }
 }
 
-const rename_channel = (req, res) => {
-    const io = socketManager.getIO()
+const rename_channel = async (req, res) => {
+    const io = socketManager.getIO();
     const channelId = req.params.channelId;
     const { newName } = req.body;
 
     if (!newName) return res.status(400).send('New channel name is required.');
 
-    db.query('UPDATE channels SET name = ? WHERE id = ?', [newName, channelId], (err, result) => {
-        if (err) {
-            return res.status(500).send('Error renaming channel.');
-        }
+    try {
+        const [result] = await db.query('UPDATE channels SET name = ? WHERE id = ?', [newName, channelId]);
 
         if (result.affectedRows === 0) {
             return res.status(404).send('Channel not found.');
         }
 
         res.status(200).send('Channel renamed successfully.');
-        io.emit('channel_rename', { id: channelId, channel_name: newName })
-    });
+        io.emit('channel_rename', { id: channelId, channel_name: newName });
+    } catch (err) {
+        console.error('Error in rename_channel:', err);
+        res.status(500).send('Error renaming channel.');
+    }
 }
 
-const channel_members = (req, res) => {
+const channel_members = async (req, res) => {
     const channelId = req.params.channelId;
-    const formattedQuery = "SELECT * FROM channel_users INNER JOIN users ON channel_users.user_id=users.id WHERE channel_users.channel_id=?"
-    db.query(formattedQuery, [channelId], (err, result) => {
-        if (err) {
-            return res.status(500).send('Error fetching users')
-        }
-        res.status(200).json(result)
-    })
+    const formattedQuery = "SELECT * FROM channel_users INNER JOIN users ON channel_users.user_id=users.id WHERE channel_users.channel_id=?";
+
+    try {
+        const [result] = await db.query(formattedQuery, [channelId]);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Error in channel_members:', err);
+        res.status(500).send('Error fetching users');
+    }
 }
 
 module.exports = {
--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,26 +1,32 @@
-const db = require('../config/database')
-const socketManager = require('../middleware/socketIo')
+const db = require('../config/database');
+const socketManager = require('../middleware/socketIo');
 
 const channel_messages = async (req, res) => {
     const channelId = req.params.channelId;
     const query = 'SELECT m.id, m.content, u.email FROM messages m JOIN users u ON m.user_id = u.id WHERE m.channel_id = ? ORDER BY m.created_at';
-    db.query(query, [channelId], (err, results) => {
-        if (err) return res.status(500).send('Error fetching messages.');
-        res.json(results);
-    });
-}
 
-const send_message = (req, res) => {
-    const io = socketManager.getIO()
+    try {
+        const [results] = await db.query(query, [channelId]);
+        res.json(results);
+    } catch (error) {
+        console.error('Error fetching messages:', error);
+        res.status(500).send('Error fetching messages.');
+    }
+};
+
+const send_message = async (req, res) => {
+    const io = socketManager.getIO();
     const channelId = req.params.channelId;
     const { content } = req.body;
     const userId = req.userId;
     const userEmail = req.userEmail;
-    if (!content.message) return res.status(400).send('Message content is required.');
+
+    if (!content || !content.message) return res.status(400).send('Message content is required.');
 
     const query = 'INSERT INTO messages (channel_id, user_id, content) VALUES (?, ?, ?)';
-    db.query(query, [channelId, userId, content.message], (err, result) => {
-        if (err) return res.status(500).send('Error saving message.');
+
+    try {
+        const [result] = await db.query(query, [channelId, userId, content.message]);
 
         const message = {
             id: result.insertId,
@@ -32,10 +38,13 @@ const send_message = (req, res) => {
 
         io.emit('newMessage', message);
         res.status(200).send(message);
-    });
-}
+    } catch (error) {
+        console.error('Error saving message:', error);
+        res.status(500).send('Error saving message.');
+    }
+};
 
 module.exports = {
     channel_messages,
     send_message
-}
+};
